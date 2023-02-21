46a9f69356ccd29a13c91552fda79cb62646decb
DESKTOP-JRDFMQ8\82106 <blehye.dev@gmail.com>
2023-02-21 17:56:40 +0900

    오타 수정

--- vue/project/src/views/1_databinding/DataBindingInputView.vue
+++ vue/project/src/views/1_databinding/DataBindingInputView.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <input type="text" v-model="userId" />
-        <buttono @click="myFunction">클릭</button>
+        <button @click="myFunction">클릭</button>
         <button @click="changeData">변경</button>
         <br>
         <input type="text" v-model="num1"/>
@@ -19,9 +19,9 @@ export default {
     components: {},
     data() {
         return {
-            userId: 'hwhan'
+            userId: 'hwhan',
             num1: 0,
-            num2: 1,
+            num2: 0,
             num3: 0,
             num4: 0
         }
