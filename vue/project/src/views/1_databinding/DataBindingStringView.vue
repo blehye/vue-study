11f7b52a3b495f2b6860d1518378995f73feb0cb
DESKTOP-JRDFMQ8\82106 <blehye.dev@gmail.com>
2023-02-21 14:53:42 +0900

    vue 설치 후 프로젝트 생성

---
+++ vue/project/src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Hello {{ userName }}</h1>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            userName: "hyewon",
+            message: "hello",
+            arr: [],
+            abj: {}
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
